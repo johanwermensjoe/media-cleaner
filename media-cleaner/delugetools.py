2fbffd2dca43cdf4f3d15b3250e16b7c55d940b9
Johan Wermensjö <johanwermensjoe@gmail.com>
2016-05-08 23:31:36 +0200

    Fixed bug in deluge command parsing.

--- media-cleaner/delugetools.py
+++ media-cleaner/delugetools.py
@@ -7,20 +7,27 @@ from subprocess import Popen, PIPE
 
 
 def has_active_torrents():
-    """Check for any active torrents on the local deluged server.
+    """ Check for any active torrents on the local deluged server.
 
     :return: True if one or more torrents are active
     :rtype: Bool
     """
+    std_output, std_err_output = '', ''
+    proc = None
     try:
         proc = Popen(["deluge-console", "info"], stdout=PIPE, stderr=PIPE)
         std_output, std_err_output = proc.communicate()
+        # Decode
+        std_output = std_output.decode("utf-8")
+        std_err_output = std_err_output.decode("utf-8")
     except (OSError, ValueError):
+        if proc is not None:
+            proc.kill()
         raise RuntimeError("deluge-console could not be found, " +
                            "make sure it is installed")
 
-    if (b'fatal' in std_output) or (
-            b'fatal' in std_err_output) or proc.returncode >= 1:
+    if ('fatal' in std_output) or (
+            'fatal' in std_err_output) or proc.returncode >= 1:
         raise RuntimeError("Status check failed, " +
                            "make sure that deluged is running")
     else:
